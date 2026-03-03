4c7eaa24938cf235b346c403cfdbf65d6d8b3b3a
Vladimir Kalinin <mr.vladimar.kalinin2000@mail.ru>
2026-03-03 15:21:33 +0400

    РÐÐеазлищзовал подключение  оработающее подключение к базе данныхÐÐ

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,15 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        try (Connection myConnection = Util.getMySQLConnection()) {
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
