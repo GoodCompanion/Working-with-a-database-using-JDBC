2e295b0493fba3130ee6ae4fd0430ce25886adc0
Vladimir Kalinin <mr.vladimar.kalinin2000@mail.ru>
2026-03-09 17:32:16 +0400

    РеализоваÐл рÐсеркÐласс UserServiceImpl

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -4,10 +4,14 @@ import jm.task.core.jdbc.util.Util;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        try (Connection myConnection = Util.getMySQLConnection()) {
+        try (Connection connection = Util.getMySQLConnection(); Statement statement = connection.createStatement()) {
+            if (!connection.isClosed()) {
+                System.out.println("Соединение с БД установлено!");
+            }
         } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
