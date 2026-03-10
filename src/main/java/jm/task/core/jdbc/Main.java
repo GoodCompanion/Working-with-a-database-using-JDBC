b4f6fb6a92c5528f25f821436238bb8d4fc82d7e
Vladimir Kalinin <mr.vladimar.kalinin2000@mail.ru>
2026-03-10 21:22:08 +0400

    Исправил кламетод cleanUsersTable в сервис класссе

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -6,18 +6,23 @@ import jm.task.core.jdbc.model.User;
 public class Main {
     public static void main(String[] args) {
         UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+
+//        System.out.println("Пытаемся создать таблицу");
+//        userDaoJDBC.createUsersTable();
+//        System.out.println("Таблица создана");
+        System.out.println("Пытаемся удалить таблицу");
+        userDaoJDBC.dropUsersTable();
+        System.out.println("Таблица удалена");
         System.out.println("Пытаемся создать таблицу");
         userDaoJDBC.createUsersTable();
         System.out.println("Таблица создана");
-        System.out.println("Пытаемся удалить таблицу");
-        userDaoJDBC.dropUsersTable();
-        System.out.println("Таблица удалена");
-        System.out.println("Пытаемся удалить таблицу");
-        userDaoJDBC.dropUsersTable();
-        System.out.println("Таблица удалена");
-//        System.out.println("Пытаемся добавить пользователя");
-//        userDaoJDBC.saveUser("Vladimir", "Kalinin", (byte) 25);
-//        System.out.println("Пользователь добавлен");
+//        System.out.println("Пытаемся удалить таблицу");
+//        userDaoJDBC.dropUsersTable();
+//        System.out.println("Таблица удалена");
+        System.out.println("Пытаемся добавить пользователя");
+        userDaoJDBC.saveUser("Vladimir", "Kalinin", (byte) 25);
+        System.out.println("Пользователь добавлен");
+
 //        System.out.println("Пытаемся добавить пользователя");
 //        userDaoJDBC.saveUser("Dmitriy", "Pavlenko", (byte) 20);
 //        System.out.println("Пользователь добавлен");
@@ -32,8 +37,12 @@ public class Main {
 //        System.out.println("Пытаемся удалить пользователя");
 //        userDaoJDBC.removeUserById(1);
 //        System.out.println("Пользователь удален");
-//        System.out.println("Пытаемся удалить всех пользователей");
-//        userDaoJDBC.cleanUsersTable();
-//        System.out.println("Все пользователи удалены");
+        System.out.println("Пытаемся удалить всех пользователей");
+        userDaoJDBC.cleanUsersTable();
+        System.out.println("Все пользователи удалены");
+        System.out.println("Пытаемся удалить всех пользователей");
+        userDaoJDBC.cleanUsersTable();
+        System.out.println("Все пользователи удалены");
+        System.out.println(userDaoJDBC.getAllUsers().size());
     }
 }
