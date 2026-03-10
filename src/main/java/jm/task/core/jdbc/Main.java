420cc6a318a5f81f6eac49fd125450345cc3528a
Vladimir Kalinin <mr.vladimar.kalinin2000@mail.ru>
2026-03-10 20:43:56 +0400

    РЕÐеализовал мет ¾д пgetAllUsers и протестировал его

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,11 +1,7 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import jm.task.core.jdbc.model.User;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,11 +9,22 @@ public class Main {
         System.out.println("Пытаемся создать таблицу");
         userDaoJDBC.createUsersTable();
         System.out.println("Таблица создана");
-//        System.out.println("Пытаемся добавить пользователя");
-//        userDaoJDBC.saveUser("vladimir", "Kalinin", (byte) 25);
-//        System.out.println("Пользователь добавлен");
-        System.out.println("Пытаемся удалить пользователя");
-        userDaoJDBC.removeUserById(1);
-        System.out.println("Пользователь удален");
+        System.out.println("Пытаемся добавить пользователя");
+        userDaoJDBC.saveUser("Vladimir", "Kalinin", (byte) 25);
+        System.out.println("Пользователь добавлен");
+        System.out.println("Пытаемся добавить пользователя");
+        userDaoJDBC.saveUser("Dmitriy", "Pavlenko", (byte) 20);
+        System.out.println("Пользователь добавлен");
+        System.out.println("Пытаемся добавить пользователя");
+        userDaoJDBC.saveUser("Zaur", "Tregulov", (byte) 35);
+        System.out.println("Пользователь добавлен");
+        System.out.println("Пытаемся получить всех пользователей");
+        for (User user : userDaoJDBC.getAllUsers()) {
+            System.out.println(user);
+        }
+        System.out.println("Все пользователи успешно получены");
+//        System.out.println("Пытаемся удалить пользователя");
+//        userDaoJDBC.removeUserById(1);
+//        System.out.println("Пользователь удален");
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,8 +3,10 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -48,7 +50,23 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        String getAllSQL = "SELECT * FROM users";
+        try (Statement statement = Util.getInstance().getConnection().createStatement()) {
+            ResultSet resultSet = statement.executeQuery(getAllSQL);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("last_name"));
+                user.setAge(resultSet.getByte("age"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            System.err.println("При получении всех пользователей произошла ошибка: ");
+            e.printStackTrace();
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
