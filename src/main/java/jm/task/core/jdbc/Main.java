ad59457e318e1a3b2b9dbf69e570de5817379810
Vladimir Kalinin <mr.vladimar.kalinin2000@mail.ru>
2026-03-13 10:00:51 +0400

    Метод main() использует сервис а не DAO

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,17 +1,18 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-        userDaoJDBC.createUsersTable();
-        userDaoJDBC.saveUser("Vladimir", "Kalinin", (byte) 25);;
-        userDaoJDBC.saveUser("Dmitriy", "Pavlenko", (byte) 20);
-        userDaoJDBC.saveUser("Zaur", "Tregulov", (byte) 35);
-        userDaoJDBC.saveUser("Maxim", "Mamedov", (byte) 14);
-        userDaoJDBC.getAllUsers();
-        userDaoJDBC.cleanUsersTable();
-        userDaoJDBC.dropUsersTable();
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Vladimir", "Kalinin", (byte) 25);
+        userService.saveUser("Dmitriy", "Pavlenko", (byte) 20);
+        userService.saveUser("Zaur", "Tregulov", (byte) 35);
+        userService.saveUser("Maxim", "Mamedov", (byte) 14);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
